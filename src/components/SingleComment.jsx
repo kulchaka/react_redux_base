0755c73f3a603029b6f638dae6122c1b6226a32d
kulchaka <kulchaka@gmail.com>
2021-12-19 17:54:27 +0100

    upgrate comment input

--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const SingleComment = ({data}) => {
   const {id, text} = data
+  const [commentText, setCommentText] = useState('')
   console.log('props Comments >', data)
+
+  useEffect(() => {
+    if(text) {
+      setCommentText(text)
+    }
+  }, [text])
+
+  const handleCommentText = (event) => {
+    setCommentText(event.target.value)
+  }
+
   return (
       <form className="comments-item">
         <div className="comments-item-delete">
           &times;
         </div>
-        <input type="text" value={text}/>
+        <input type="text" value={commentText} onChange={handleCommentText}/>
         <input type="submit" hidden/>
       </form>
   );
